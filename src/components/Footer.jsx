1395b5bcb289f3f5134f5210624b349c412fdb7c
Nathan Laviolette <67888122+growtoups@users.noreply.github.com>
2024-07-15 17:51:33 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,10 +5,9 @@ import { footerLinks } from "../constants";
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerlink) => (
-          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
+          <div key={footerlink.title} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
             <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
               {footerlink.title}
             </h4>
@@ -20,7 +19,7 @@ const Footer = () => (
                     index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
                   }`}
                 >
-                  {link.name}
+                  <a href={link.link}>{link.name}</a>
                 </li>
               ))}
             </ul>
@@ -30,15 +29,11 @@ const Footer = () => (
     </div>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-    <img
-          src={logo}
-          alt="DiscreetShare"
-          className="w-[50px] h-[50px]"
-        /> <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-   Copyright Ⓒ 2024 DiscreetShare. All Rights Reserved.
+      <img src={logo} alt="DiscreetShare" className="w-[50px] h-[50px]" />
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
+        Copyright Ⓒ 2024 DiscreetShare. All Rights Reserved.
       </p>
-
-<div></div>
+      <div></div>
     </div>
   </section>
 );
